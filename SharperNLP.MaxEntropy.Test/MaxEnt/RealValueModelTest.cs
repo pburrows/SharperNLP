ada7328da304a4dd1c3022dfe700ded0cfd72c3f
patrick <pburrows@burrowscorp.com>
2011-02-02 10:14:41 -0500

    Created project structure for SharperNLP.MaxEntropy and SharperNLP.MaxEntropy.Test

---
+++ SharperNLP.MaxEntropy.Test/MaxEnt/RealValueModelTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharperNLP.MaxEntropy.Test.MaxEnt
+{
+    class RealValueModelTest
+    {
+    }
+}
