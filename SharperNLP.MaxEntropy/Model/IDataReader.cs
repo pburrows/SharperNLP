b1addbd7e33ca52ee9c1d2723b6df573d1cdd3eb
RePierre <petru.rebeja@gmail.com>
2011-03-13 14:00:41 +0200

    Converted DataReader interface to IDataReader.

---
+++ SharperNLP.MaxEntropy/Model/IDataReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharperNLP.MaxEntropy.Model
+{
+    /// <summary>
+    /// Represents a data reader.
+    /// </summary>
+    public interface IDataReader
+    {
+        /// <summary>
+        /// Reads a double.
+        /// </summary>
+        /// <returns>The read double value.</returns>
+        double ReadDouble();
+
+        /// <summary>
+        /// Reads an integer.
+        /// </summary>
+        /// <returns>The integer value.</returns>
+        int ReadInt();
+
+        /// <summary>
+        /// Reads a string in the UTF.
+        /// </summary>
+        /// <returns>The string.</returns>
+        string ReadUTF();
+    }
+}
